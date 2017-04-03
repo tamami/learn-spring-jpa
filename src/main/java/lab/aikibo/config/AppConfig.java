30c1885f12006edff144b7e99b23b5cdf6cbbf74
tamami <tamami.oka@gmail.com>
2017-04-03 16:12:23 +0700

    berhasil get datasource

--- src/main/java/lab/aikibo/config/AppConfig.java
+++ src/main/java/lab/aikibo/config/AppConfig.java
@@ -1,12 +1,33 @@
 package lab.aikibo.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 /**
  * Created by tamami on 03/04/17.
  */
 @Configuration
-@EnableAutoConfiguration
+@EnableJpaRepositories("lab.aikibo.repo")
 public class AppConfig {
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        ds.setUrl(env.getRequiredProperty("jdbc.url"));
+        ds.setUsername(env.getRequiredProperty("jdbc.username"));
+        ds.setPassword(env.getRequiredProperty("jdbc.password"));
+
+        return ds;
+    }
 }
